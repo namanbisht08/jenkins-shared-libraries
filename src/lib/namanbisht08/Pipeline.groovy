e9660abe247bbf48da11aaf80ce2fcdc93e8bccb
Naman Singh Bisht <56620879+namanbisht08@users.noreply.github.com>
2025-01-15 17:48:10 +0530

    Update Pipeline.groovy

--- src/lib/namanbisht08/Pipeline.groovy
+++ src/lib/namanbisht08/Pipeline.groovy
@@ -16,6 +16,21 @@ def ImagePublishECR(String REGION, String repo_name, String DockerfileName, Stri
 	DockerImage.push("${version}")
 }
 
+def Creating_updated_task_defination(String REGION, String TaskDefinationFamily, String repo_name, String version) {
+    println "lib.namanbisht08: Creating an Updated Revision of Task Definition"
+
+    def taskDefFile = "${TaskDefinationFamily}-task-def.json"
+    sh "aws ecs describe-task-definition --task-definition ${TaskDefinationFamily} --region ${REGION} > ${taskDefFile}"
+    sh "jq '.taskDefinition.containerDefinitions[0].image = \"${repo_name}:${version}\"' ${taskDefFile} > ${taskDefFile}"
+    sh "aws ecs register-task-definition --family ${TaskDefinationFamily} --cli-input-json file://${taskDefFile} --region ${REGION}"
+    sh "rm -f ${taskDefFile}"
+
+    println "====== New Task Definition Updated and Registered Successfully ======"
+}
+
+
+
+
 def ImageTagCheck(String repo_name, String version) {
         int status = sh(
                         script: "docker manifest inspect ${repo_name}:${version}",
